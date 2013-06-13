45485071882b46c41f72cc72ebeb4cfab8d8d527
Anthony Scopatz <scopatz@gmail.com>
2013-06-13 00:32:03 -0500

    Added fix for errant colon in parameters list

--- docs/conf.py
+++ docs/conf.py
@@ -138,6 +138,7 @@ html_logo = '_static/HooverJoven.jpg'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = "numpy_friendly.css"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
