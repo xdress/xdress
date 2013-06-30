15b9d79b288e28c1da914d644c38ea4a36864719
Anthony Scopatz <scopatz@gmail.com>
2013-06-30 17:39:35 -0500

    added logo

--- setup.py
+++ setup.py
@@ -6,10 +6,28 @@ import subprocess
 
 import configure
 
-xdress_logo = """"""
+xdress_logos = ["""                                         
+XXXXXXX       XXXXXXXDDDDDDDDDDDDD        
+X:::::X       X:::::XD::::::::::::DDD     
+X:::::X       X:::::XD:::::::::::::::DD   
+X::::::X     X::::::XDDD:::::DDDDD:::::D  
+XXX:::::X   X:::::XXX  D:::::D    D:::::D 
+   X:::::X X:::::X     D:::::D     D:::::D
+    X:::::X:::::X      D:::::D     D:::::D
+     X:::::::::X       D:::::D     D:::::D
+     X:::::::::X       D:::::D     D:::::D
+    X:::::X:::::X      D:::::D     D:::::D
+   X:::::X X:::::X     D:::::D     D:::::D
+XXX:::::X   X:::::XXX  D:::::D    D:::::D 
+X::::::X     X::::::XDDD:::::DDDDD:::::D  
+X:::::X       X:::::XD:::::::::::::::DD   
+X:::::X       X:::::XD::::::::::::DDD     
+XXXXXXX       XXXXXXXDDDDDDDDDDDDD        
+""",
+]
 
 def main_body():
-    print(xdress_logo)
+    print(xdress_logos[0])
     configure.setup()
 
 def main():
