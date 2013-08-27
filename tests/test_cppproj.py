ecb5c9fce8412f3d6b76d3ad2f6855a43c4814fb
Anthony Scopatz <scopatz@gmail.com>
2013-08-26 21:57:06 -0500

    further along the testing platform

--- tests/test_cppproj.py
+++ tests/test_cppproj.py
@@ -8,11 +8,18 @@ from nose.tools import assert_true, assert_equal
 
 from xdress.astparsers import PARSERS_AVAILABLE
 
+if sys.version_info[0] >= 3:
+    basestring = str
+
 PROJDIR = os.path.abspath("cppproj")
 
 def check_cmd(args):
-    print("TESTING: Running Command:\n\n{0}\n\n".format(" ".join(args)))
-    return subprocess.call(args, cwd=PROJDIR, stderr=subprocess.STDOUT)
+    if not isinstance(args, basestring):
+        args = " ".join(args)
+    print("TESTING: running command in {0}:\n\n{1}\n\n".format(PROJDIR, args))
+    rtn = subprocess.call(args, shell=True, cwd=PROJDIR, stderr=subprocess.STDOUT)
+    assert_equal(rtn, 0)
+    return rtn
 
 # Because we want to guarentee build and test order, we can only have one 
 # master test function which generates the individual tests.
@@ -22,19 +29,24 @@ def test_all():
     cases = [{'parsers': p} for p in parsers]
 
     cwd = os.getcwd()
+    base = os.path.dirname(cwd)
     pyexec = sys.executable
-    xdexec = os.path.join(cwd, 'scripts', 'xdress')
+    xdexec = os.path.join(base, 'scripts', 'xdress')
     instdir = os.path.join(PROJDIR, 'install')
 
     cmds = [
-        ['PYTHONPATH="{0}"'.format(cwd), pyexec, xdexec, '--debug'],
+        ['PYTHONPATH="{0}"'.format(base), pyexec, xdexec, '--debug'],
         [pyexec, 'setup.py', 'install', '--prefix="{0}"'.format(instdir), '--', '--'],
         ]
 
     for case in cases:
         parser = case['parsers']
-        if not PARSERS_AVAILABL[parser]:
+        if not PARSERS_AVAILABLE[parser]:
             continue
+        rtn = 1
         for cmd in cmds:
             rtn = yield check_cmd, cmd
-            assert_equal(rtn, 0)
+            if rtn != 0:
+                break  # don't execute further commands
+        if rtn != 0:
+            break  # don't try further cases
