4f85a21a240e97e0cd30b3e181f27a64e0c8fec7
Matthew Gidden <matthew.gidden@gmail.com>
2014-04-03 10:52:22 -0500

    adds test dir support for dtypes

--- xdress/dtypes.py
+++ xdress/dtypes.py
@@ -693,7 +693,8 @@ class XDressPlugin(Plugin):
         fname = os.path.join(rc.packagedir, rc.dtypes_module)
         ensuredirs(fname)
         testname = 'test_' + rc.dtypes_module
-        testname = os.path.join(rc.packagedir, 'tests', testname)
+        testroot = rc.testroot or rc.packagedir
+        testname = os.path.join(testroot, 'tests', testname)
         ensuredirs(testname)
         genfiles(rc.dtypes, fname=fname, testname=testname, package=rc.package, 
                  ts=rc.ts, verbose=rc.verbose)
